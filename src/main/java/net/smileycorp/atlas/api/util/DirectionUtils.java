5e8c9924ae685d3dc2064e240e833623653d2eee
Smileycorp <callum.j.longbottom@gmail.com>
2021-02-08 00:10:22 +0000

    added more functionality to positional utilities

---
+++ src/main/java/net/smileycorp/atlas/api/util/DirectionUtils.java
@@ -0,0 +1,38 @@
+package net.smileycorp.atlas.api.util;
+
+import java.util.Random;
+
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+
+public class DirectionUtils {
+
+	public static EnumFacing getRandomDirection(Random rand) {
+		return getDirection(rand.nextInt(4));
+	}
+
+	public static EnumFacing getDirection(int direction) {
+		switch (direction) {
+			case 1:
+				return EnumFacing.SOUTH;
+			case 2:
+				return EnumFacing.EAST;
+			case 3:
+				return EnumFacing.WEST;
+			default:
+				return EnumFacing.NORTH;
+		}
+	}
+	
+	public static int getMeta(EnumFacing facing) {
+		if (facing==EnumFacing.UP||facing==EnumFacing.DOWN) {
+			return facing.getIndex()+4;
+		}
+		return facing.getIndex()-2;
+	}
+	
+	public static BlockPos getPos(BlockPos pos, EnumFacing facing) {
+		return new BlockPos(pos).add(facing.getDirectionVec());
+	}
+
+}
