6a2d1f5404fb55cdad032cbaed2a1f516cf429bc
Smileycorp <callum.j.longbottom@gmail.com>
2021-07-24 21:27:59 +0100

    add decoders

--- src/main/java/net/smileycorp/atlas/api/network/SimpleMessageDecoder.java
+++ src/main/java/net/smileycorp/atlas/api/network/SimpleMessageDecoder.java
@@ -1,6 +1,5 @@
 package net.smileycorp.atlas.api.network;
 
-import java.lang.reflect.ParameterizedType;
 import java.util.function.Function;
 
 import net.minecraft.network.INetHandler;
@@ -9,11 +8,16 @@ import net.minecraft.network.PacketBuffer;
 
 public class SimpleMessageDecoder<T extends IPacket<INetHandler>> implements Function<PacketBuffer, T> {
 
+	protected final Class<T> clazz;
+
+	public SimpleMessageDecoder(Class<T> clazz) {
+		this.clazz=clazz;
+	}
+
 	@Override
-	@SuppressWarnings("unchecked")
 	public T apply(PacketBuffer buf) {
 		try {
-			T message = ((Class<T>)((ParameterizedType)((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]).getRawType()).newInstance();
+			T message = clazz.newInstance();
 			message.readPacketData(buf);
 			return message;
 		} catch (Exception e) {
