2605110f44ec67df22effc6972bf07cbebe5b3cc
Smileycorp <callum.j.longbottom@gmail.com>
2021-08-25 22:30:01 +0100

    fixed functionality of data utils

---
+++ src/main/java/net/smileycorp/atlas/api/data/DataType.java
@@ -0,0 +1,67 @@
+package net.smileycorp.atlas.api.data;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import net.minecraft.nbt.CompoundNBT;
+
+import com.google.gson.JsonElement;
+
+
+public class DataType<T extends Comparable<T>> {
+
+	private static Map<String, DataType<?>> registry = new HashMap<String, DataType<?>>();
+
+	public static DataType<Byte> BYTE = new DataType<Byte>("byte", Byte.class, (json)->json.getAsByte(), (nbt, key) -> nbt.getByte(key));
+	public static DataType<Short> SHORT = new DataType<Short>("short", Short.class, (json)->json.getAsShort(), (nbt, key) -> nbt.getShort(key));
+	public static DataType<Integer> INT = new DataType<Integer>("int", Integer.class, (json)->json.getAsInt(), (nbt, key) -> nbt.getInt(key));
+	public static DataType<Long> LONG = new DataType<Long>("long", Long.class, (json)->json.getAsLong(), (nbt, key) -> nbt.getLong(key));
+	public static DataType<Float> FLOAT = new DataType<Float>("float", Float.class, (json)->json.getAsFloat(), (nbt, key) -> nbt.getFloat(key));
+	public static DataType<Double> DOUBLE = new DataType<Double>("double", Double.class, (json)->json.getAsDouble(), (nbt, key) -> nbt.getDouble(key));
+	public static DataType<String> STRING = new DataType<String>("string", String.class, (json)->json.getAsString(), (nbt, key) -> nbt.getString(key));
+	public static DataType<Boolean> BOOLEAN = new DataType<Boolean>("boolean", Boolean.class, (json)->json.getAsBoolean(), (nbt, key) -> nbt.getBoolean(key));
+
+	private final String name;
+	private final Class<?> clazz;
+	private final Function<JsonElement, T> jsonReader;
+	private final BiFunction<CompoundNBT, String, T> nbtReader;
+
+	private DataType(String name, Class<T> clazz, Function<JsonElement, T> jsonReader, BiFunction<CompoundNBT, String, T> nbtReader) {
+		this.name=name;
+		this.clazz=clazz;
+		this.jsonReader=jsonReader;
+		this.nbtReader=nbtReader;
+		registry.put(name, this);
+	}
+
+	public Class<?> getType() {
+		return clazz;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public T readFromJson(JsonElement element) {
+		return jsonReader.apply(element);
+	}
+
+	public T readFromNBT(CompoundNBT nbt, String key) {
+		return nbtReader.apply(nbt, key);
+	}
+
+	public static DataType<?> of(String name) {
+		if (registry.containsKey(name)) return registry.get(name);
+		return null;
+	}
+
+	public static DataType<?> of(Class<?> clazz) {
+		for (DataType<?> type : registry.values()) {
+			if (type.getType()==clazz) return (DataType<?>) type;
+		}
+		return null;
+	}
+
+}
--- src/main/java/net/smileycorp/atlas/api/data/NBTExplorer.java
+++ src/main/java/net/smileycorp/atlas/api/data/NBTExplorer.java
@@ -2,45 +2,72 @@ package net.smileycorp.atlas.api.data;
 
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.INBT;
-import net.minecraft.nbt.NumberNBT;
-import net.minecraft.nbt.StringNBT;
+import net.minecraft.nbt.ListNBT;
 
 public class NBTExplorer<T extends Comparable<T>> {
 
 	private final String target;
-	private final Class<T> returnType;
+	private final DataType<T> returnType;
 
-	public NBTExplorer (String target, Class<T> returnType) {
+	public NBTExplorer (String target, DataType<T> returnType) {
 		this.target = target;
 		this.returnType = returnType;
 	}
 
-	@SuppressWarnings("unchecked")
 	public T findValue(CompoundNBT compound) throws Exception {
 		String[] directory = target.split("\\.");
 		INBT nbt = compound;
 		for (int i = 0; i < directory.length; i++) {
-			if (i == directory.length-1) {
-				Object value;
-				if (nbt instanceof NumberNBT) {
-					value = ((NumberNBT) nbt).getAsNumber();
-				} else if (nbt instanceof StringNBT) {
-					value =((StringNBT)nbt).getString();
+			try {
+				if (nbt instanceof CompoundNBT) {
+					if (i == directory.length-1) {
+						T value = returnType.readFromNBT((CompoundNBT) nbt, directory[i]);
+						if (value == null) {
+							throw new Exception("Value " + directory[i] + "is not of type " + returnType.getType());
+						}
+						return value;
+					} else {
+						nbt = ((CompoundNBT) nbt).get(directory[i]);
+					}
+				} else if (nbt instanceof ListNBT) {
+					INBT nextNBT = null;
+					for (INBT tag : ((ListNBT)nbt)) {
+						if (tag instanceof CompoundNBT) {
+							if (((CompoundNBT) tag).getString("Name").equals(directory[i])) {
+								nextNBT = tag;
+								break;
+							}
+						}
+					}
+					if  (nextNBT==null) {
+						throw new Exception("NBTList " + nbt + " does not contain \"Name\":\"" + directory[i] + "\"");
+					} else {
+						nbt = nextNBT;
+					}
 				} else {
-					throw new Exception("Cannot find value");
+					throw new Exception("Value " + directory[i] + " is not an applicable type or cannot be found as nbt is " + nbt);
 				}
-				if (returnType.isAssignableFrom(value.getClass())) {
-					return (T) value;
-				} else {
-					throw new Exception("Value is not of type " + returnType);
+			} catch (Exception e) {
+				StringBuilder builder = new StringBuilder();
+				for (int j = 0; j < directory.length; j++) {
+					if (i == j) {
+						builder.append(">"+directory[j]+"<");
+					} else {
+						builder.append(directory[j]);
+					}
+					if (j < directory.length-1) {
+						builder.append("\\.");
+					}
 				}
-			} else if (nbt instanceof CompoundNBT) {
-				nbt = ((CompoundNBT) nbt).get(directory[i]);
-			} else {
-				throw new Exception("Cannot find value");
+				throw new Exception(builder.toString() + " " + e.getMessage(), e.getCause());
 			}
 		}
 		return null;
+	}
+
+	@Override
+	public String toString() {
+		return target;
 	}
 
 }
