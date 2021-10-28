9702e7a8bfe26b2dad4a9c3455568e7641f1f31a
Smileycorp <callum.j.longbottom@gmail.com>
2021-10-29 00:40:18 +0100

    added WeightedOutput changes from 1.12.2

--- src/main/java/net/smileycorp/atlas/api/recipe/WeightedOutputs.java
+++ src/main/java/net/smileycorp/atlas/api/recipe/WeightedOutputs.java
@@ -2,6 +2,7 @@ package net.smileycorp.atlas.api.recipe;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -10,34 +11,34 @@ import java.util.Random;
 
 public class WeightedOutputs<T> {
 
-	private final List<Entry<T, Integer>> entries;
-	private final int deafaultTries;
-	
+	protected final List<Entry<T, Integer>> entries = new ArrayList<Entry<T, Integer>>();
+	protected int defaultTries;
+
 	public WeightedOutputs(Map<T, Integer> entries) {
 		this(1, new ArrayList<Entry<T, Integer>>(entries.entrySet()));
 	}
-	
-	public WeightedOutputs(int deafultTries, Map<T, Integer> entries) {
-		this(deafultTries, new ArrayList<Entry<T, Integer>>(entries.entrySet()));
+
+	public WeightedOutputs(int defaultTries, Map<T, Integer> entries) {
+		this(defaultTries, entries.entrySet());
 	}
 
-	public WeightedOutputs(int deafaultTries, List<Entry<T, Integer>> entries) {
-		this.deafaultTries = deafaultTries;
-		this.entries = entries;
+	public WeightedOutputs(int defaultTries, Collection<Entry<T, Integer>> entries) {
+		this.defaultTries = defaultTries;
+		this.entries.addAll(entries);
 	}
 
 	public int getAmount() {
-		return deafaultTries;
+		return defaultTries;
 	}
 
 	public List<Entry<T, Integer>> getTable() {
 		return entries;
 	}
-	
+
 	public boolean isEmpty() {
 		return entries.isEmpty();
 	}
-	
+
 	public T getResult(Random rand) {
 		return getResults(rand, 1).get(0);
 	}
@@ -46,7 +47,45 @@ public class WeightedOutputs<T> {
 		return getResults(rand, 1);
 	}
 
-	private List<T> getResults(Random rand, int tries) {
+	public void clear() {
+		entries.clear();
+	}
+
+	public int getWeight(T t) {
+		for (Entry<T, Integer> entry : entries) {
+			if (entry.getKey() == t) entry.getValue();
+		}
+		return 0;
+	}
+
+	public boolean removeEntry(T t) {
+		boolean result = false;
+		for (Entry<T, Integer> entry : entries) {
+			if (entry.getKey() == t) {
+				entries.remove(entry);
+				result = true;
+			}
+		}
+		return result;
+	}
+
+	public void setDefaultTries(int value) {
+		defaultTries = value;
+	}
+
+	public void addEntry(T t, int weight) {
+		entries.add(new SimpleEntry<T, Integer>(t, weight));
+	}
+
+	public void addEntries(Collection<Entry<T, Integer>> entries) {
+		this.entries.addAll(entries);
+	}
+
+	public void addEntries(Map<T, Integer> entries) {
+		this.entries.addAll(entries.entrySet());
+	}
+
+	public List<T> getResults(Random rand, int tries) {
 		List<T> list = new ArrayList<T>();
 		List<Entry<T, Integer>> mappedEntries = new ArrayList<Entry<T, Integer>>();
 		int max = 0;
@@ -56,7 +95,7 @@ public class WeightedOutputs<T> {
 		}
 		if (max>0) {
 			Collections.reverse(mappedEntries);
-			for(int i=0; i<(tries*deafaultTries);i++){
+			for(int i=0; i<(tries*defaultTries);i++){
 				int result = rand.nextInt(max);
 				for(Entry<T, Integer> entry : mappedEntries) {
 					if (result>=entry.getValue()) {
