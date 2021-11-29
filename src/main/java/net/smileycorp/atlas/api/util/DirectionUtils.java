22a7071c5989fbc368dec6403a80ca1ee6aa1cc7
Smileycorp <callum.j.longbottom@gmail.com>
2021-11-29 22:03:53 +0000

    rebuilt to fix issue with changes in 1.1.3b

--- src/main/java/net/smileycorp/atlas/api/util/DirectionUtils.java
+++ src/main/java/net/smileycorp/atlas/api/util/DirectionUtils.java
@@ -144,7 +144,7 @@ public class DirectionUtils {
 
 	public static BlockPos getClosestLoadedPos(World world, BlockPos basepos, Vector3d  direction, double radius, int maxlight, int minlight) {
 		BlockPos pos = world.getHeightmapPos(Type.WORLD_SURFACE, basepos.offset(direction.x*radius, 0, direction.z*radius));
-		while (world.getChunkSource().getChunkNow(pos.getX() >> 4, pos.getY() >> 4) == null || !isBrightnessAllowed(world, basepos, maxlight, minlight)) {
+		while (world.getChunkSource().getChunkNow(pos.getX() >> 4, pos.getZ() >> 4) == null || !isBrightnessAllowed(world, basepos, maxlight, minlight)) {
 			if (radius==0) return basepos;
 			radius--;
 			pos = world.getHeightmapPos(Type.WORLD_SURFACE, basepos.offset(direction.x*radius, 0, direction.z*radius));
