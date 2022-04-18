c6560eae9d06efac8b50cbd5976d741c93fe5b78
Smileycorp <callum.j.longbottom@gmail.com>
2022-04-18 19:20:30 +0100

    add functions to save and read items from nbt
    add IOngoingEvent functionality from 1.16

--- src/main/java/net/smileycorp/atlas/api/IOngoingEvent.java
+++ src/main/java/net/smileycorp/atlas/api/IOngoingEvent.java
@@ -1,15 +1,14 @@
 package net.smileycorp.atlas.api;
 
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.level.Level;
 
-public interface IOngoingEvent {
-	
+public interface IOngoingEvent<T> {
+
 	public void readFromNBT(CompoundTag nbt);
-	
+
 	public CompoundTag writeToNBT(CompoundTag nbt);
-	
-	public void update(Level level);
-	
-	public boolean isActive(Level level);
+
+	public void update(T world);
+
+	public boolean isActive(T world);
 }
--- src/main/java/net/smileycorp/atlas/api/util/DataUtils.java
+++ src/main/java/net/smileycorp/atlas/api/util/DataUtils.java
@@ -2,6 +2,12 @@ package net.smileycorp.atlas.api.util;
 
 import java.util.UUID;
 
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.world.item.ItemStack;
+
 public class DataUtils {
 
 	public static boolean isValidUUID(String uuid) {
@@ -11,6 +17,18 @@ public class DataUtils {
 		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	public static void saveItemsToNBT(CompoundTag nbt, NonNullList<ItemStack> items) {
+		ListTag nbtItems = new ListTag();
+		for (ItemStack stack : items) nbtItems.add(stack.save(new CompoundTag()));
+		nbt.put("Inventory", nbtItems);
+	}
+
+	public static NonNullList<ItemStack> readItemsFromNBT(CompoundTag nbt) {
+		NonNullList<ItemStack> items = NonNullList.create();
+		for (Tag tag : nbt.getList("Inventory", 10)) if (tag instanceof CompoundTag) items.add(ItemStack.of((CompoundTag) tag));
+		return items;
 	}
 
 }
