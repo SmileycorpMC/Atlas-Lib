aab07a2aec6bd62b980daaa440bd4850526e59a1
Smileycorp <callum.j.longbottom@gmail.com>
2024-12-09 22:29:00 +0000

    add check to make sure snowed grass has no tint

--- src/main/java/net/smileycorp/atlas/api/client/colour/BlockGrassColour.java
+++ src/main/java/net/smileycorp/atlas/api/client/colour/BlockGrassColour.java
@@ -1,5 +1,6 @@
 package net.smileycorp.atlas.api.client.colour;
 
+import net.minecraft.block.BlockGrass;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.util.math.BlockPos;
@@ -9,6 +10,7 @@ public class BlockGrassColour implements IBlockColor {
 
 	@Override
 	public int colorMultiplier(IBlockState state, IBlockAccess world, BlockPos pos, int tintIndex) {
+		if (state.getBlock() instanceof BlockGrass) if (state.getValue(BlockGrass.SNOWY)) return 0xFFFFFF;
 		return world.getBiome(pos).getGrassColorAtPos(pos);
 	}
 
