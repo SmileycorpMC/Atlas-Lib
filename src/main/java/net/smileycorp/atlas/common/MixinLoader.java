236aa19a67fc1d090b81719103809cd45f4f1135
Smileycorp <callum.j.longbottom@gmail.com>
2025-06-03 18:22:25 +0100

    make mod bootstrap mixinextras

---
+++ src/main/java/net/smileycorp/atlas/common/MixinLoader.java
@@ -0,0 +1,39 @@
+package net.smileycorp.atlas.common;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import com.llamalad7.mixinextras.MixinExtrasBootstrap;
+import javax.annotation.Nullable;
+import java.util.Map;
+
+public class MixinLoader implements IFMLLoadingPlugin {
+
+    public MixinLoader() {
+        MixinBootstrap.init();
+        MixinExtrasBootstrap.init();
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {}
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
