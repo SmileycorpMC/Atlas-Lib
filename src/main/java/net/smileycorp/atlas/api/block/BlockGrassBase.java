fad786d46ff63a64aa53787b8f85c93444aa8351
Smileycorp <callum.j.longbottom@gmail.com>
2025-06-03 20:36:03 +0100

    fix grass issues

--- src/main/java/net/smileycorp/atlas/api/block/BlockGrassBase.java
+++ src/main/java/net/smileycorp/atlas/api/block/BlockGrassBase.java
@@ -3,22 +3,40 @@ package net.smileycorp.atlas.api.block;
 import net.minecraft.block.BlockDirt;
 import net.minecraft.block.BlockGrass;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.UseHoeEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import javax.annotation.Nullable;
 import java.util.Locale;
 import java.util.Random;
 import java.util.function.Function;
 
 public class BlockGrassBase extends BlockGrass implements BlockProperties {
-    
-    private final Function<IBlockState, IBlockState> base;
-    
+
+    private static boolean registered;
+    private final Function<IBlockState, IBlockState> base, farmland;
+
     public BlockGrassBase(String name, String modid, CreativeTabs tab, Function<IBlockState, IBlockState> base) {
+       this(name, modid, tab, base, null);
+    }
+
+    public BlockGrassBase(String name, String modid, CreativeTabs tab, Function<IBlockState, IBlockState> base, @Nullable Function<IBlockState, IBlockState> farmland) {
         super();
         name = name.toLowerCase(Locale.US);
         setRegistryName(new ResourceLocation(modid, name.toLowerCase(Locale.US)));
@@ -26,10 +44,20 @@ public class BlockGrassBase extends BlockGrass implements BlockProperties {
         setCreativeTab(tab);
         setSoundType(SoundType.PLANT);
         this.base = base;
+        this.farmland = farmland;
+        if (!registered && farmland != null) {
+            registered = true;
+            MinecraftForge.EVENT_BUS.register(new EventHandler());
+        }
     }
-    
+
     public BlockGrassBase(String name, String modid, SoundType sound, float h, float r, String tool, int level, CreativeTabs tab, Function<IBlockState, IBlockState> base) {
-        this(name, modid, tab, base);
+        this(name, modid, sound, h, r, tool, level, tab, base, null);
+    }
+
+    public BlockGrassBase(String name, String modid, SoundType sound, float h, float r, String tool, int level, CreativeTabs tab,
+                          Function<IBlockState, IBlockState> base, @Nullable Function<IBlockState, IBlockState> farmland) {
+        this(name, modid, tab, base, farmland);
         setResistance(r);
         setHardness(h);
         setHarvestLevel(tool, level);
@@ -58,5 +86,36 @@ public class BlockGrassBase extends BlockGrass implements BlockProperties {
                     world.setBlockState(blockpos, ((GrowsGrass) iblockstate1.getBlock()).getGrass(state, rand));
         }
     }
-    
+
+    @Override
+    public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction, IPlantable plant) {
+        EnumPlantType type = plant.getPlantType(world, pos.offset(direction));
+        if (type == EnumPlantType.Plains) return true;
+        if (type == EnumPlantType.Cave) return state.isSideSolid(world, pos, EnumFacing.UP);
+        if (type == EnumPlantType.Beach) for (EnumFacing facing : EnumFacing.HORIZONTALS)
+            if (world.getBlockState(pos.offset(facing)).getMaterial() == Material.WATER) return true;
+        return false;
+    }
+
+    private static class EventHandler {
+
+        @SubscribeEvent
+        public void useHoe(UseHoeEvent event) {
+            World world = event.getWorld();
+            BlockPos pos = event.getPos();
+            IBlockState state = world.getBlockState(pos);
+            if (!(state.getBlock() instanceof BlockGrassBase)) return;
+            BlockGrassBase block = (BlockGrassBase) state.getBlock();
+            if (block.farmland == null) return;
+            if (!world.isAirBlock(pos.up())) return;
+            event.setResult(Event.Result.ALLOW);
+            EntityPlayer player = event.getEntityPlayer();
+            world.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1, 1);
+            if (world.isRemote) return;
+            world.setBlockState(pos, block.farmland.apply(state), 11);
+            event.getCurrent().damageItem(1, player);
+        }
+
+    }
+
 }
