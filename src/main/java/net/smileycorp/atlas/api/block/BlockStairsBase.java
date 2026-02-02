d34718b431933b95697c7373248c5e4068094b85
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-02-02 14:31:45 +0000

    fix stairs hardness

--- src/main/java/net/smileycorp/atlas/api/block/BlockStairsBase.java
+++ src/main/java/net/smileycorp/atlas/api/block/BlockStairsBase.java
@@ -4,12 +4,19 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockStairs;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class BlockStairsBase extends BlockStairs {
+
+	private final IBlockState state;
 
 	public BlockStairsBase(Block base) {
 		this(base.getRegistryName().getResourcePath(), base.getDefaultState());
@@ -17,6 +24,7 @@ public class BlockStairsBase extends BlockStairs {
 	
 	public BlockStairsBase(String name, IBlockState state) {
 		super(state);
+		this.state = state;
 		useNeighborBrightness = true;
 		Block base = state.getBlock();
 		setSoundType(base.getSoundType(state, null, null, null));
@@ -35,6 +43,16 @@ public class BlockStairsBase extends BlockStairs {
 	@Override
 	public int getFireSpreadSpeed(IBlockAccess world, BlockPos pos, EnumFacing facing) {
 		return world.getBlockState(pos).getMaterial() == Material.WOOD ? 5 : super.getFireSpreadSpeed(world, pos, facing);
+	}
+
+	@Override
+	public float getBlockHardness(IBlockState state, World world, BlockPos pos) {
+		return this.state.getBlockHardness(world, pos);
+	}
+
+	@Override
+	public float getExplosionResistance(World world, BlockPos pos, @Nullable Entity entity, Explosion explosion) {
+		return this.state.getBlock().getExplosionResistance(world, pos, entity, explosion);
 	}
 
 }
